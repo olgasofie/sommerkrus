61410bdd58c24511899e750f8564ca879bc7abf3
olgasofie <138442132+olgasofie@users.noreply.github.com>
2023-07-04 14:43:50 +0200

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+name = input("enter a name")
+place = input("enter place")
+season = input ("enter season")
+changer = input("enter changer")
+funny = input ("enter funny")
+
+print(f"\n {name}went to {place}during{season}went to {changer} during{funny} went to a joke.\n")
+ 
